3dc2e363ac7766ad68a1b0516b9cee01a2eb5ddd
MANGWA Alexandre <alexandre.mangwa@soprasteria.com>
2022-07-05 15:07:12 +0200

    Définition de pkranger en tant que service en container Docker et écriture du 1er Dockerfole

---
+++ api.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+print(app)
+
+@app.get("/")
+async def hello_world():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000
+                )
